45758390679854fda3b81aaf6865f64c527cba67
lijiawei04 <lijiawei04@baijiahulian.com>
2021-06-13 20:06:52 +0800

    [feat] 「罗马数字」和「阿拉伯数字」转换

---
+++ src/main/java/com/example/leetcode/editor/cn/IntegerToRoman.java
@@ -0,0 +1,135 @@
+package com.example.leetcode.editor.cn;
+
+//罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。 
+//
+// 
+//字符          数值
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000 
+//
+// 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做 XXVII, 即为 XX + V + I
+//I 。 
+//
+// 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5
+// 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况： 
+//
+// 
+// I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。 
+// X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+// C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。 
+// 
+//
+// 给你一个整数，将其转为罗马数字。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: num = 3
+//输出: "III" 
+//
+// 示例 2: 
+//
+// 
+//输入: num = 4
+//输出: "IV" 
+//
+// 示例 3: 
+//
+// 
+//输入: num = 9
+//输出: "IX" 
+//
+// 示例 4: 
+//
+// 
+//输入: num = 58
+//输出: "LVIII"
+//解释: L = 50, V = 5, III = 3.
+// 
+//
+// 示例 5: 
+//
+// 
+//输入: num = 1994
+//输出: "MCMXCIV"
+//解释: M = 1000, CM = 900, XC = 90, IV = 4. 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= num <= 3999 
+// 
+// Related Topics 数学 字符串 
+// 👍 636 👎 0
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class IntegerToRoman {
+
+    public static void main(String[] args) {
+        Solution solution = new IntegerToRoman().new Solution();
+        System.out.println(solution.intToRoman(900));
+        System.out.println(solution.intToRoman2(900));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String intToRoman2(int num) {
+        Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            Integer value = entry.getKey();
+            String str = entry.getValue();
+            while (num >= value) {
+                sb.append(str);
+                num -= value;
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public String intToRoman(int num) {
+        int[] key = new int[]{ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] value = new String[]{ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < key.length; i++) {
+            while (num >= key[i]) {
+                sb.append(value[i]);
+                num -= key[i];
+            }
+        }
+        return sb.toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/example/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/example/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -91,11 +91,13 @@ class Solution {
         int i = 0, j = 0;
         while (i < s.length() && j < s.length()) {
             if (!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
+                set.add(s.charAt(j));
+                j++;
                 result = Math.max(result, j - i);
             }
             else {
-                set.remove(s.charAt(i++));
+                set.remove(s.charAt(i));
+                i++;
             }
         }
         return result;
